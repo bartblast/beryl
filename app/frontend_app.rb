c9dcc3daa9fef8c7914b327c87f5849b7129e52d
Bart Blast <bart@bartblast.com>
2018-09-21 12:37:10 +0200

    Implement basic HTML DSL

--- app/frontend_app.rb
+++ app/frontend_app.rb
@@ -5,21 +5,41 @@ require 'virtual_dom'
 
 puts 'Wow, running opal!'
 
-element = {
-  type: 'div',
-  props: { id: 'container' },
-  children: [
-    { type: 'input', props: { value: 'foo', type: 'text' } },
-    { type: 'a', props: { href: '/bar' } },
-    {
-      type: 'span',
-      props: {},
-      children: [
-        { type: 'text', props: { nodeValue: 'Foo' } }
-      ]
-    }
-  ]
-}
+def div(props = {}, &children)
+  node('div', props, children ? children.call : [])
+end
+
+def input(props = {}, &children)
+  node('input', props, children ? children.call : [])
+end
+
+def link(props = {}, &children)
+  node('a', props, children ? children.call : [])
+end
+
+def node(type, props = {}, children)
+  {
+    type: type,
+    props: props,
+    children: children
+  }
+end
+
+def span(props = {}, &children)
+  node('span', props, children ? children.call : [])
+end
+
+def text(value, props = {}, &children)
+  node('text', props.merge(nodeValue: value), children ? children.call : [])
+end
+
+element = div(id: 'container') {[
+  input(value: 'foo', type: 'text'),
+  link(href: '/bar'),
+  span() {[
+    text('Foo')
+  ]}
+]}
 
 def onload(&block)
   `window.onload = block;`
