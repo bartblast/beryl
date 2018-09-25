67bd6cdb3152fa40fcabcc53a3256058d7f3f7c5
Bart Blast <bart@bartblast.com>
2018-09-25 16:48:16 +0200

    Implement composite widget DSL

--- lib/beryl/widget.rb
+++ lib/beryl/widget.rb
@@ -1,3 +1,5 @@
+require 'beryl/utils/constantizer'
+
 module Beryl
   class Widget
     attr_reader :children
@@ -18,6 +20,15 @@ module Beryl
 
     def column(*args, &block)
       @children << build(:column, *args, &block)
+    end
+
+    def method_missing(name, *args, &block)
+      constantized = Beryl::Utils::Constantizer.(name.to_s)
+      children = args.any? ? constantized.new.render(*args) : constantized.new.render
+      raise SyntaxError.new("Widget #{name} should return only one element (use row or column)") if children.size > 1
+      @children += children
+    rescue NoMethodError
+      raise NameError.new("There is no such widget: #{name}")
     end
 
     def row(*args, &block)
