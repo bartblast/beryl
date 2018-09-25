5d1ad35707d568ca2ddf67427716b8f9ff633238
Bart Blast <bart@bartblast.com>
2018-09-25 14:48:29 +0200

    Simplify widget DSL

--- lib/beryl/widget.rb
+++ lib/beryl/widget.rb
@@ -1,7 +1,35 @@
-require 'beryl/widgets/elements'
-
 module Beryl
   class Widget
-    include Beryl::Widgets::Elements
+    attr_reader :children
+
+    def initialize
+      @children = []
+    end
+
+    def build(type, *args, &block)
+      element = Widget.new
+      element.instance_eval(&block)
+      {
+        type: type,
+        props: args,
+        children: element.children
+      }
+    end
+
+    def column(*args, &block)
+      @children << build(:column, *args, &block)
+    end
+
+    def row(*args, &block)
+      @children << build(:row, *args, &block)
+    end
+
+    def text(string, props = nil)
+      @children << {
+        type: :text,
+        value: string,
+        props: props
+      }
+    end
   end
 end
