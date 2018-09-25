1543b52850c87139c52065f1703dea57f92e1237
Bart Blast <bart@bartblast.com>
2018-09-25 14:21:07 +0200

    Refactor var names

--- lib/virtual_dom.rb
+++ lib/virtual_dom.rb
@@ -18,8 +18,8 @@ class VirtualDOM
   private
 
   def add_event_listeners(element, dom, event_loop)
-    listener_props = element[:props].select { |key, _value| listener?(key) }
-    listener_props.each do |key, value|
+    listeners = element[:props].select { |key, _value| listener?(key) }
+    listeners.each do |key, value|
       event_type = key.downcase[2..-1]
       dom.addEventListener(event_type, lambda { event_loop.push(value); event_loop.process })
     end
@@ -30,8 +30,8 @@ class VirtualDOM
   end
 
   def set_attributes(element, dom)
-    attribute_props = element[:props].reject { |key, _value| listener?(key) }
-    attribute_props.each { |key, value| dom[key] = value }
+    attributes = element[:props].reject { |key, _value| listener?(key) }
+    attributes.each { |key, value| dom[key] = value }
   end
 
   def update_dom(parent_dom, dom, replace)
