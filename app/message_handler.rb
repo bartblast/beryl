30bd9aaccda3bc5c35e00be85169e0c77bd4cf68
Bart Blast <bart@bartblast.com>
2018-10-17 22:14:16 +0200

    Update demo code

--- app/message_handler.rb
+++ app/message_handler.rb
@@ -4,17 +4,17 @@ class MessageHandler < Beryl::MessageHandler
   def handle(state, message, payload)
     case message
 
-    when :IncrementClicked
-      state.merge(counter: state[:counter] + 1, random: payload[:random])
+    when :increment_clicked
+      state.merge counter: state[:counter] + 1, random: payload[:random]
 
     when :LoadClicked
       [state, :FetchData, key_1: 1, key_2: 2]
 
     when :SendCommandToPort
-      [state, :SomePortMessage, :abc_port, key_1: 111]
+      [state, :PortCommand, key_1: 111]
 
     when :LoadSuccess
-      state.merge(content: payload[:data])
+      state.merge content: payload[:data]
 
     else
       [state]
--- app/widgets/homepage.rb
+++ app/widgets/homepage.rb
@@ -6,7 +6,7 @@ class Homepage < Beryl::Widget
     column :fill_width, :fill_height do
       row :fill_width do
         column proportional_width: 2 do
-          button on_click: [:IncrementClicked, random: SecureRandom.uuid] do
+          button on_click: [:increment_clicked, random: SecureRandom.uuid] do
             row do
               text 'Click'
               text ' me!'
@@ -32,15 +32,27 @@ class Homepage < Beryl::Widget
           text 'ROW 2 COL 3'
         end
       end
-      row :fill_width do
+      row :fill_width, height: 200, background: [255, 0, 0] do
         column :fill_width do
           text 'ROW 3 COL 1'
         end
         column width: 400 do
           text 'ROW 3 COL 2'
+          on_left do
+            column :fill_height, background: [120, 120, 0] do
+              text 'text 1'
+              text 'text 2'
+            end
+          end
         end
         column :fill_width do
           text 'ROW 3 COL 3'
+        end
+        below do
+          text 'below', background: [0, 0, 255]
+        end
+        above do
+          text 'above', background: [0, 255, 0]
         end
       end
     end
